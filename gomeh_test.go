85474decd12719bf1a7c1fa8c190382e36dc9c31
Hunter Horsman <kariudo@gmail.com>
2015-01-25 23:00:52 -0500

    coverage status badge & travis ts
    troubleshooting issue with ts env var

--- gomeh_test.go
+++ gomeh_test.go
@@ -15,16 +15,19 @@ func readKey() string {
 	// If API key file exists
 	if _, err := os.Stat(f); err == nil {
 		// Read API key from file
+		fmt.Println("Reading from file.")
 		buf, err := ioutil.ReadFile(f)
 		if err != nil {
 			log.Fatal(err)
 		}
 		key = string(buf)
 	} else {
+		fmt.Println("Reading from file.")
 		// Read API key from env (for travis)
 		key = os.Getenv("meh_apikey")
 	}
 	// Trim the string to remove any whitespace or linebreaks
+	fmt.Println(key)
 	return strings.Trim(key, " \n")
 }
 
